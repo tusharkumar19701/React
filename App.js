ee669704d1e2778230262ab1d3246f3f166dacf7
Tushar Kumar <tusharkumar19701@gmail.com>
2023-07-30 19:02:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("h1", {
+    id: "title",
+}, "Namaste Everyone");
+
+const heading2 = React.createElement("h2", {
+    id: "title",
+}, "Heading 2");
+
+const container = React.createElement(
+    "div", 
+    {
+    id: "container"
+    },
+ [heading, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
